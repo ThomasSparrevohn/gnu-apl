f4e986702902b1e4af1b905a18c69da95b9b46fb
Jürgen Sauermann <j_sauermann@savannah.gnu.org>
2023-04-28 13:22:52 +0000

    add apl-library

--- config.h
+++ config.h
@@ -2,10 +2,10 @@
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* the SVN version of Archive.cc */
-#define ARCHIVE_SVN " 146"
+#define ARCHIVE_SVN " 211"
 
 /* How and when GNU APL was configured */
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / SVN: 193", "2023-04-25 16:28:12 UTC", "Linux 5.15.0-70-generic x86_64", "default ./configure options"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / SVN: 218", "2023-04-28 13:03:52 UTC", "Linux 5.15.0-70-generic x86_64", "default ./configure options"
 
 /* Define to 1 iff pthread_setaffinity_np() is available */
 #define HAVE_AFFINITY_NP 1
