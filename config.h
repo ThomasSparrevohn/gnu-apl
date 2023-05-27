ce6513c3dc04a2e11138dfa18e6ff3afab566b77
Jürgen Sauermann <j_sauermann@savannah.gnu.org>
2023-05-27 15:09:33 +0000

    fix pgk-config python3

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
 #define ARCHIVE_SVN " 211"
 
 /* How and when GNU APL was configured */
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / SVN: 334", "2023-05-27 14:19:46 UTC", "Linux 5.15.0-72-generic x86_64", "default ./configure options"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / SVN: 335", "2023-05-27 14:59:54 UTC", "Linux 5.15.0-72-generic x86_64", "default ./configure options"
 
 /* Define to 1 iff pthread_setaffinity_np() is available */
 #define HAVE_AFFINITY_NP 1
